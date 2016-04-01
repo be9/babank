f0100bcb30710868e47dc0b63a840954d38b8be7
oleg dashevskii <olegdashevskii@gmail.com>
2016-04-01 19:18:36 +0600

    Fix zero amount in factory

--- spec/factories/transfers.rb
+++ spec/factories/transfers.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :transfer do
     association :source, factory: :account
     association :target, factory: :account
-    amount { BigDecimal.new(rand(1000)) / 100 }  # 0 .. 9.99
+    amount { BigDecimal.new(1+rand(1000)) / 100 }  # 0 .. 9.99
     date { Date.today }
     retracted_on nil
   end
